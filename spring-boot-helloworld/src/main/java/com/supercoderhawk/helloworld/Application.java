1aeb8e31de84048a4bf7faa562fbbc3cd0325bbf
SuperCoderX <xyb930826@gmail.com>
2017-05-04 10:40:27 +0800

    init commit

---
+++ spring-boot-helloworld/src/main/java/com/supercoderhawk/helloworld/Application.java
@@ -0,0 +1,21 @@
+package com.supercoderhawk.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xyb-C308 on 2017/5/3.
+ */
+@RestController
+@SpringBootApplication
+public class Application {
+  @RequestMapping("/")
+  public String index() {
+    return "Hello,world";
+  }
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Application.class, args);
+  }
+}
