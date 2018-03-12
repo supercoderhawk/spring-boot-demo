6307872121bc76c615cd9e44eb197edaedefc135
supercoderhawk <supercoderhawk@gmail.com>
2018-03-12 23:28:55 +0800

    add spring boot main function

--- spring-boot-mybatis/src/main/java/com/supercoderhawk/demo/Application.java
+++ spring-boot-mybatis/src/main/java/com/supercoderhawk/demo/Application.java
@@ -1,9 +1,16 @@
 package com.supercoderhawk.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by xyb-C308 on 2017/5/6.
  */
 
+@SpringBootApplication
 public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
